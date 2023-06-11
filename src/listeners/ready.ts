6ea2ab9be3c5c8779fa80ab8dc6889d793540408
PenPow <josh@penpow.dev>
2023-06-11 16:14:24 +0100

    refactor(listeners): rename ready event class

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -2,7 +2,7 @@ import { ApplyOptions } from "@sapphire/decorators";
 import { Listener } from "@sapphire/framework";
 
 @ApplyOptions<Listener.Options>({ once: true })
-export class UserEvent extends Listener {
+export class ReadyEvent extends Listener {
   public run() {
     this.container.logger.info("Ready!");
   }
