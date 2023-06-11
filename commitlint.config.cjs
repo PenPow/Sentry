d7acc8499ba638562eb641ce4ea402ecfd3875fb
PenPow <josh@penpow.dev>
2023-06-11 09:51:48 +0100

    chore(commitlint): allow art as a commit type

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,7 @@
 module.exports = {
-	extends: ['@commitlint/config-conventional']
+	extends: ['@commitlint/config-conventional'],
+
+	rules: {
+		type: ["feat", "fix", "docs", "chore", "ci", "refactor", "art"]
+	}
 }
