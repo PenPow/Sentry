082d49ad2d09217c4320209adc58cc6ad1dd7bc5
PenPow <josh@penpow.dev>
2023-06-17 09:58:32 +0100

    chore: add org slug to sentry release script

--- scripts/buildSentryRelease.sh
+++ scripts/buildSentryRelease.sh
@@ -1,8 +1,8 @@
-VERSION=`./node_modules/.bin/sentry-cli releases propose-version`
+VERSION=`./node_modules/.bin/sentry-cli releases propose-version --org sentry-discord --project bot`
 
-./node_modules/.bin/sentry-cli releases set-commits "$VERSION" --auto
+./node_modules/.bin/sentry-cli releases set-commits "$VERSION" --auto --org sentry-discord --project bot
 
-./node_modules/.bin/sentry-cli sourcemaps inject dist/
-./node_modules/.bin/sentry-cli sourcemaps upload dist/
+./node_modules/.bin/sentry-cli sourcemaps inject dist/ --org sentry-discord --project bot
+./node_modules/.bin/sentry-cli sourcemaps upload dist/ --org sentry-discord --project bot
 
-./node_modules/.bin/sentry-cli releases finalize "$VERSION"
+./node_modules/.bin/sentry-cli releases finalize "$VERSION" --org sentry-discord --project bot
