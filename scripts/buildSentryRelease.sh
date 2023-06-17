3a420f3bc2826633e1948e9e4b4efe74493a2828
PenPow <josh@penpow.dev>
2023-06-17 10:08:44 +0100

    chore(sentry): associate sourcemaps with release

--- scripts/buildSentryRelease.sh
+++ scripts/buildSentryRelease.sh
@@ -2,7 +2,7 @@ VERSION=`./node_modules/.bin/sentry-cli releases propose-version --org sentry-di
 
 ./node_modules/.bin/sentry-cli releases set-commits "$VERSION" --auto --org sentry-discord --project bot
 
-./node_modules/.bin/sentry-cli sourcemaps inject dist/ --org sentry-discord --project bot
-./node_modules/.bin/sentry-cli sourcemaps upload dist/ --org sentry-discord --project bot
+./node_modules/.bin/sentry-cli sourcemaps inject dist/ --release=$VERSION --org sentry-discord --project bot
+./node_modules/.bin/sentry-cli sourcemaps upload dist/ --release=$VERSION --org sentry-discord --project bot
 
 ./node_modules/.bin/sentry-cli releases finalize "$VERSION" --org sentry-discord --project bot
