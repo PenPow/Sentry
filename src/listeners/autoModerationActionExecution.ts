4b8249a1e3e72ba4206a8fa484dfa12aeea96592
PenPow <josh@penpow.dev>
2023-06-24 15:29:23 +0100

    refactor: use message create for domain scanning

--- src/listeners/autoModerationActionExecution.ts
+++ src/listeners/autoModerationActionExecution.ts
@@ -5,38 +5,6 @@ import normalizeUrl from "normalize-url";
 export class AutoModerationActionExecutionEvent extends Listener<typeof Events.AutoModerationActionExecution> {
   public async run(event: AutoModerationActionExecution) {
     const { content, guild, user } = event;
-
-    // thanks discord
-    // you created an automod system that allows regexes
-    // you didnt add functionality to your system however to execute the same regex multiple times, and so it executes once per message
-    // this opens up the potential exploit of sending a benign domain initially, and then the malicious one
-    // using your built in matched_content allows this
-    // therefore, we are now using cpu time to parse out ALL domains myself
-    // with a separate regex because you use rust regex
-    // to perform the simple matching functionality that should be already implemented
-    const matches = content.match(/(?:https?:\/\/)?\S{2,}\.\S{2,18}\/?\S*/gi);
-
-    for (const match of matches ?? []) {
-      const domain = new URL(normalizeUrl(match, { stripHash: true, stripAuthentication: true, normalizeProtocol: true, stripTextFragment: true }));
-
-      if (this.container.utilities.phishing.check(domain.hostname)) {
-        await this.container.utilities.moderation.createCase(
-          event.guild,
-          {
-            reason: "Malicious Domain Detected",
-            guildId: guild.id,
-            duration: null,
-            moderatorId: this.container.client.user!.id,
-            action: "Ban",
-            userId: user!.id,
-            userName: user!.username,
-            referenceId: null,
-          },
-          true
-        );
-
-        break; // Exit so we dont check the further domains - they are already banned
-      }
-    }
+    
   }
 }
---
+++ src/listeners/messageCreate.ts
@@ -0,0 +1,34 @@
+import { Events, Listener } from "@sapphire/framework";
+import { Message } from "discord.js";
+import normalizeUrl from "normalize-url";
+
+export class MessageCreateListener extends Listener<typeof Events.MessageCreate> {
+  public async run(message: Message) {
+    if (!message.inGuild()) return;
+
+    const matches = message.content.match(/(?:https?:\/\/)?\S{2,}\.\S{2,18}\/?\S*/gi);
+
+    for (const match of matches ?? []) {
+      const domain = new URL(normalizeUrl(match, { stripHash: true, stripAuthentication: true, normalizeProtocol: true, stripTextFragment: true }));
+
+      if (this.container.utilities.phishing.check(domain.hostname)) {
+        await this.container.utilities.moderation.createCase(
+          message.guild,
+          {
+            reason: "Malicious Domain Detected",
+            guildId: message.guild.id,
+            duration: null,
+            moderatorId: this.container.client.user!.id,
+            action: "Ban",
+            userId: message.author!.id,
+            userName: message.author!.username,
+            referenceId: null,
+          },
+          true
+        );
+
+        break; // Exit so we dont check the further domains - they are already banned
+      }
+    }
+  }
+}
