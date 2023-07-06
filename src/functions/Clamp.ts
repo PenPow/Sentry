8fe822acd91d94a5f083dbb3e612098d7c860970
PenPow <josh@penpow.dev>
2023-07-06 14:01:25 +0100

    refactor: purge the old files
    out with the old, in with the new

--- src/functions/Clamp.ts
+++
@@ -1,5 +0,0 @@
-export function clamp<T extends unknown[]>(array: T, length: number): T {
-  array.length = Math.min(array.length, length);
-
-  return array;
-}
--- src/types/ProcessEnv.d.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/consistent-type-definitions */
-declare namespace NodeJS {
-  export interface ProcessEnv {
-    DOPPLER_SECRETS_KEY: string;
-    NODE_ENV: "PRODUCTION" | "DEVELOPMENT";
-    GIT_COMMIT: string;
-  }
-}
