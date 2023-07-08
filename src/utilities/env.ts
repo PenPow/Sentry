5d726b984ce65ccf6516b60e78b82ab4abc8c45a
PenPow <josh@penpow.dev>
2023-07-08 14:02:36 +0100

    feat(about): add about command

--- src/utilities/env.ts
+++ src/utilities/env.ts
@@ -5,7 +5,12 @@ import { inspect } from "util";
 const schema = s.object({
     DISCORD_TOKEN: s.string.regex(/(?<mfaToken>mfa\.[a-z0-9_-]{20,})|(?<basicToken>[a-z0-9_-]{23,28}\.[a-z0-9_-]{6,7}\.[a-z0-9_-]{27})/i),
     DEVELOPMENT_GUILD_ID: s.string.regex(/^(?<id>\d{17,20})$/),
-    PRISMA_ENCRYPTION_KEY: s.string.lengthEqual(57)
+    PRISMA_ENCRYPTION_KEY: s.string.lengthEqual(57),
+
+    // NOTE: Injected at compile time
+    // eslint-disable-next-line max-len
+    NODE_VERSION: s.string.regex(/^([0-9]|[1-9][0-9]*)\.([0-9]|[1-9][0-9]*)\.([0-9]|[1-9][0-9]*)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?$/),
+    GIT_COMMIT: s.string.lengthEqual(40)
 });
 
 export function loadEnv(logger?: Logger<ILogObj>) {
