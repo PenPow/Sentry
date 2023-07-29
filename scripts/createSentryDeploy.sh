d2a965c678a72530a983019a199a051e24e1f964
PenPow <josh@penpow.dev>
2023-07-29 15:02:58 +0100

    ci: revert my hard work

--- scripts/createSentryDeploy.sh
+++
@@ -1,3 +0,0 @@
-VERSION=`./node_modules/.bin/sentry-cli releases propose-version --org sentry-discord --project bot`
-
-./node_modules/.bin/sentry-cli releases deploys "$VERSION" -e PRODUCTION --org sentry-discord --project bot
